86f79cbc7bf52e07f459dcd2498f3f0cf73c9ac8
hnsol <masatora.bd5@gmail.com>
2026-06-16 21:39:47 +0900

    Switch menu bar icon to archive box

--- Sources/QuickDrop/main.swift
+++ Sources/QuickDrop/main.swift
@@ -459,7 +459,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         statusItem.button?.image = NSImage(
-            systemSymbolName: "sparkle.text.clipboard",
+            systemSymbolName: "archivebox.fill",
             accessibilityDescription: "ClipScylf"
         )
         let menu = NSMenu()
